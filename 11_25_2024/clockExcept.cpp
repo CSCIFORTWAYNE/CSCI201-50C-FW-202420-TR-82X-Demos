d19490aed2c8b290a5727f61c8c35fe31264ef9c
ACat32 <99211070+ACat32@users.noreply.github.com>
2024-11-28 00:08:56 -0500

    Update clockExcept.cpp
    added invalid_min and invalid_sec

--- 11_25_2024/clockExcept.cpp
+++ 11_25_2024/clockExcept.cpp
@@ -3,3 +3,7 @@
 invalid_AmPmType::invalid_AmPmType(std::string tod) : invalid_argument(tod + " is not a valid time of day.") {}
 
 invalid_Hour::invalid_Hour(std::string errMsg) : invalid_argument(errMsg) {}
+
+invalid_Min::invalid_Min(std::string errMsg) : invalid_argument(errMsg) {}
+
+invalid_Sec::invalid_Sec(std::string errMsg) : invalid_argument(errMsg) {}
